9166fc82fa9e6d5d8380749c03042cc804ed3df6
Lukas Pirl <git@lukas-pirl.de>
2018-05-15 10:21:00 +0200

    moved static/js/* → static/

---
+++ simple-slides-dark/static/slides.js
@@ -0,0 +1,4 @@
+require(['order!modernizr.custom.45394', 'order!hammer',
+         'order!slide-controller', 'order!slide-deck',
+         'order!slide-deck-instantiate'],
+        function(someModule) {});
