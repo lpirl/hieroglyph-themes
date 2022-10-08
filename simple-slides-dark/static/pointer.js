3b7f0a3e7cd9b9ae4e1757780c62ebdab6c199bd
Lukas Pirl <git@lukas-pirl.de>
2022-10-08 13:41:22 +0200

    make mouse pointer a red dot
    hides after timeout

---
+++ simple-slides-dark/static/pointer.js
@@ -0,0 +1,52 @@
+/**
+ * Make the mouse pointer look a bit like a laser pointer.
+ * Code quality "make it work", improvements welcome.
+ *
+ * @authors Lukas Pirl
+ */
+
+(function(window) {
+
+  var timeout = 3000;
+  var color = 'rgba(255, 0, 0, 1)';
+  var size = .5;
+  var size_unit = 'rem';
+  var pointer = document.createElement('div');
+  pointer.style.position = 'fixed';
+  pointer.style.top = - 2 * size + size_unit;
+  pointer.style.left = pointer.style.top;
+  pointer.style.width = size + size_unit;
+  pointer.style.height = pointer.style.width;
+  pointer.style.marginLeft = '-' + size / 2 + size_unit;
+  pointer.style.marginTop = pointer.style.marginLeft;
+  pointer.style.backgroundColor = color;
+  pointer.style.borderRadius = '50%';
+  pointer.style.zIndex = 1000000;
+  pointer.style.transition = 'none';
+
+  /* enables click (also selecting text) "through" red dot cursor */
+  pointer.style.pointerEvents = "none";
+
+  var parent = document.querySelector('body:not(.popup)')
+  parent.appendChild(pointer);
+  parent.style.cursor = "none";
+
+  var mouseTimer = null;
+  function onMouseMove(e){
+      if (mouseTimer) {
+          window.clearTimeout(mouseTimer);
+      } else {
+          pointer.style.display = 'initial';
+      }
+      pointer.style.top = e.clientY + "px";
+      pointer.style.left = e.clientX + "px";
+      mouseTimer = window.setTimeout(function() {
+          pointer.style.display = 'none';
+          mouseTimer = null;
+      }
+      , timeout);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+
+
+})(window);
